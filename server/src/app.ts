63d733d9996ef71d5717cf52f6b2eaa3032e5cf8
NicolaLovo <nicolalovo@gmail.com>
2025-05-13 17:15:50 +0200

    Added cors policies

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import expressListEndpoints from 'express-list-endpoints';
 import authRouter from './routes/auth/authRouter';
@@ -6,8 +7,9 @@ import authRouter from './routes/auth/authRouter';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.use('/api/v1/', authRouter);
+app.use('/api/v1/auth/', authRouter);
 
 app.get('/test', (req, res) => {
   res.send(JSON.stringify(expressListEndpoints(app), null, 2));
