4c1c9f5faef4f2d23c6d1b2d9dfdf069cee326a2
NickLech <niccololechthaler@gmail.com>
2025-05-15 13:55:50 +0200

    Commented line

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import router from '@/router'
 import { getAuth, signOut } from 'firebase/auth'
-import SurveyCreateForm from './components/surveys/SurveyCreateForm/SurveyCreateForm.vue'
+// import SurveyCreateForm from './components/surveys/SurveyCreateForm/SurveyCreateForm.vue'
 import { APP_ROUTES } from './constants/APP_ROUTES'
 import { useUserStore } from './stores/useUserStore'
 
@@ -33,9 +33,9 @@ const logout = async () => {
       </div>
     </header>
 
-    <!--<router-view />-->
+    <router-view />
 
-    <SurveyCreateForm />
+    <!-- <SurveyCreateForm /> -->
   </div>
 </template>
 
