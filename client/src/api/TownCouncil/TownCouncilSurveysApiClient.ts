89e93cd9bbf4432597e405ad7bbb3d9a0636442f
NicolaLovo <nicolalovo@gmail.com>
2025-05-17 13:39:39 +0200

    Added api client for list endpoint

--- client/src/api/TownCouncil/TownCouncilSurveysApiClient.ts
+++ client/src/api/TownCouncil/TownCouncilSurveysApiClient.ts
@@ -1,3 +1,4 @@
+import { API_URL } from '@/constants/API_URL'
 import { HTTP_TMRESPONSES } from '@/constants/HTTP_TMRESPONSES'
 import { TmResponse } from '@/types/common/utils/TmResponse'
 import { Survey } from '@/types/survey/Survey'
@@ -18,6 +19,58 @@ export class TownCouncilSurveysApiClient extends ApiClientChildren {
         status: 'error',
         data: {
           message: 'Error during survey creation',
+        },
+      }
+    }
+  }
+
+  /**
+   * List the surveys saved in the system
+   */
+  public async list(queries: {
+    /**
+     * 0-based index of the first item in the current page.
+     *
+     * @default 0
+     */
+    pageIndex?: string
+    /**
+     * Number of items to include in the current page.
+     *
+     * @default 10
+     */
+    pageSize?: string
+  }): Promise<
+    TmResponse<{
+      surveys: Survey[]
+      metadata: {
+        /**
+         * Total number of items that satisfy the query.
+         */
+        totalCount: number
+      }
+    }>
+  > {
+    try {
+      const baseUrl = `${API_URL}/api/v1/townCouncil/surveys`
+      const urlWithQueries = this.appendQueriesToUrl(baseUrl, queries)
+
+      type ReturnType = TmResponse<{
+        surveys: Survey[]
+        metadata: {
+          totalCount: number
+        }
+      }>
+
+      const response = await this.httpClient.get<ReturnType>(urlWithQueries)
+
+      return response ?? HTTP_TMRESPONSES.error
+    } catch (error) {
+      console.error('List Surveys error:', error)
+      return {
+        status: 'error',
+        data: {
+          message: 'Error in List Surveys request',
         },
       }
     }
--- client/src/api/base/ApiClientChildren.ts
+++ client/src/api/base/ApiClientChildren.ts
@@ -12,4 +12,42 @@ export abstract class ApiClientChildren {
     this.jwtToken = jwtToken
     this.httpClient = new HttpClient({ jwtToken })
   }
+
+  /**
+   * Append queries to a URL
+   *
+   * Note that if I append multiple values for the same key, it will append them as separate query parameters
+   * Express will parse them as an array
+   *
+   * @example
+   *
+   * /api/v1/userProjects?keys=1&keys=2&keys=3 will be parsed as { keys: [1, 2, 3] }
+   */
+  protected appendQueriesToUrl(url: string, queries: Record<string, string | number | boolean>) {
+    const urlWithQueries = new URL(url)
+    Object.entries(queries).forEach(([key, value]) => {
+      if (value === undefined) {
+        return
+      }
+
+      if (Array.isArray(value)) {
+        value.forEach((val) => {
+          urlWithQueries.searchParams.append(key, val.toString())
+        })
+        return
+      }
+
+      const stringifiedValue = ((): string => {
+        switch (typeof value) {
+          case 'string':
+            return value
+          case 'number':
+          case 'boolean':
+            return value.toString()
+        }
+      })()
+      urlWithQueries.searchParams.append(key, stringifiedValue)
+    })
+    return urlWithQueries.toString()
+  }
 }
