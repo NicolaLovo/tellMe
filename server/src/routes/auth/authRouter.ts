d06608a192bb48b4e12434e6a23fc935ae2d93c3
NicolaLovo <nicolalovo@gmail.com>
2025-05-17 10:39:41 +0200

    Update citizen register route path

--- server/src/routes/auth/authRouter.ts
+++ server/src/routes/auth/authRouter.ts
@@ -1,13 +1,12 @@
 import { Router } from 'express';
-import { registerCitizenController } from '../../controllers/auth/registerCitizenController';
 import { loginController } from '../../controllers/auth/loginController';
-
+import { registerCitizenController } from '../../controllers/auth/registerCitizenController';
 
 const authRouter = Router();
 
 // Each route is mapped to a controller function that handles the logic for that HTTP method and path
-authRouter.post('/register/citizen', registerCitizenController);       // POST register a citizen
-authRouter.post('/login', loginController);                            // POST login a citizen
+authRouter.post('/citizen', registerCitizenController); // POST register a citizen
+authRouter.post('/login', loginController); // POST login a citizen
 
-// Export this router so is available in app.ts 
+// Export this router so is available in app.ts
 export default authRouter;
