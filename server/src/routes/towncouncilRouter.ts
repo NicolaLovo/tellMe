a7490f2a44e247d6d53e038aeb57deeb281bd2a5
NicolaLovo <nicolalovo@gmail.com>
2025-05-17 22:42:17 +0200

    Removed duplicated townCouncilRouter

--- server/src/routes/towncouncilRouter.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { createSurveyController } from '../controllers/townCouncil/surveys/createSurveyController';
-const towncouncilRouter = express.Router();
-
-towncouncilRouter.post('/surveys', createSurveyController);
-
-export default towncouncilRouter;
