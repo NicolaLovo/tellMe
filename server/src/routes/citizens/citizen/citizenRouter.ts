5ff93a148c41c9b599babe5b051ec7a787285a9e
Kappa <keith.leoni03@gmail.com>
2025-05-27 15:31:12 +0200

    Setup routing to createSurveyAnswerController

--- server/src/routes/citizens/citizen/citizenRouter.ts
+++ server/src/routes/citizens/citizen/citizenRouter.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
-import { readSurveyAnswerController } from '../../../controllers/citizen/surveys/readSurveyAnswerController';
+import { createSurveyAnswerController } from '../../../controllers/citizen/surveys/createSurveyAnswerController';
 import { listCitizenSurveysController } from '../../../controllers/citizen/surveys/listCitizenSurveysController';
+import { readSurveyAnswerController } from '../../../controllers/citizen/surveys/readSurveyAnswerController';
 
 const citizenRouter = Router();
 
 citizenRouter.get('/:uid/surveys', listCitizenSurveysController);
 citizenRouter.get('/:uid/surveys/:surveyId/answer', readSurveyAnswerController);
+citizenRouter.post(
+  '/:uid/surveys/:surveyId/answer',
+  createSurveyAnswerController,
+);
 
 export default citizenRouter;
