c58fc8d4d14fbbc687d10ee0bf20fe6855c1941a
Kappa <keith.leoni03@gmail.com>
2025-05-27 15:28:51 +0200

    Setup createSurveyAnswerConrtoller

---
+++ server/src/controllers/citizen/surveys/createSurveyAnswerController.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from 'express';
+import { SurveyAnswerModel } from '../../../database/survey/SurveyAnswerSchema';
+import { TmResponse } from '../../../types/common/utils/TmResponse';
+import { SurveyAnswer } from '../../../types/survey/answer/SurveyAnswer';
+
+interface ReqParams {
+  surveyId: string;
+  uid: string;
+}
+
+type ResBody = TmResponse<{
+  answerId: string;
+}>;
+
+interface ReqBody {
+  surveyAnswer: SurveyAnswer;
+}
+
+export const createSurveyAnswerController = async (
+  req: Request<ReqParams, ResBody, ReqBody>,
+  res: Response<ResBody>,
+): Promise<void> => {
+  try {
+    const { surveyAnswer } = req.body;
+
+    const exists = await SurveyAnswerModel.findOne({
+      _id: {
+        surveyId: surveyAnswer._id.surveyId,
+        uid: surveyAnswer._id.uid,
+      },
+    });
+
+    if (exists) {
+      res.status(409).json({
+        status: 'error',
+        data: {
+          message: 'User already answered this survey.',
+        },
+      });
+      return;
+    }
+
+    const created = await SurveyAnswerModel.create(surveyAnswer);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        answerId: created._id.toString(),
+      },
+    });
+  } catch (error) {
+    console.error('Error creating survey answer:', error);
+    res.status(500).json({
+      status: 'error',
+      data: {
+        message: 'Internal server error',
+      },
+    });
+  }
+};
