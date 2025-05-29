c8948488f71baa2071ee12a202e26562856b6dc8
NicolaLovo <nicolalovo@gmail.com>
2025-05-29 12:12:57 +0200

    Setup code coverage

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'jest';
 
 const config: Config = {
   verbose: true,
+  collectCoverage: true,
   coverageDirectory: '<rootDir>/coverage',
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 };
