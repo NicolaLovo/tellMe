40b0d45b7a05fabe53480ea2a2daa4d780acb40e
Kappa <keith.leoni03@gmail.com>
2025-06-02 09:56:11 +0200

    Modified UI survey results with question and options text

--- client/src/components/surveys/display/TownCouncilSurveyResultTable.vue
+++ client/src/components/surveys/display/TownCouncilSurveyResultTable.vue
@@ -4,7 +4,6 @@ import { onMounted, reactive } from 'vue'
 
 import { ApiClient } from '@/api/ApiClient'
 import { useUserStore } from '@/stores/useUserStore'
-import Card from 'primevue/card'
 import ProgressBar from 'primevue/progressbar'
 
 // Define the props for the component, which includes the survey ID
@@ -15,17 +14,21 @@ const props = defineProps<{
 // Define the survey result data structure
 const surveyResults = reactive<SurveyResult>({
   surveyId: 'rtnwrn',
+  surveyTitle: 'Titolo del sondaggio',
   results: [
     {
       questionId: 'aaaaaaaa',
+      questionText: 'Domanda 1',
       options: [
         {
-          optionId: 'bbbbb',
+          optionId: 'opzione1',
           votes: 5,
+          optionText: 'a',
         },
         {
-          optionId: 'cccccc',
+          optionId: 'opzione2',
           votes: 9,
+          optionText: 'b',
         },
       ],
     },
@@ -43,6 +46,7 @@ const fetchSurveyResults = async () => {
     })
     if (response.status === 'success') {
       surveyResults.surveyId = response.data.surveyResults.surveyId
+      surveyResults.surveyTitle = response.data.surveyResults.surveyTitle
       surveyResults.results = response.data.surveyResults.results
       console.log('Risultati del sondaggio caricati con successo:', surveyResults)
     } else {
@@ -59,34 +63,38 @@ function calculatePercentage(votes: number, options: { optionId: string; votes: 
   return total > 0 ? Math.round((votes / total) * 100) : 0
 }
 
+// Function to get total number of responses (based on first question's total votes, since all questions should have the same number of responses)
+function getTotalResponses(): number {
+  if (surveyResults.results.length === 0) return 0
+  return surveyResults.results[0].options.reduce((sum, opt) => sum + opt.votes, 0)
+}
+
 // Fetch survey results when the component is mounted
 onMounted(fetchSurveyResults)
 </script>
 
 <template>
   <div class="p-4">
-    <h2 class="text-2xl font-bold mb-4">Risultati del sondaggio</h2>
-    <Card class="mb-4">
-      <template #title> Sondaggio</template>
-      <template #content>
-        <div v-if="surveyResults && surveyResults.results">
-          <p>Numero di domande con risposta: {{ surveyResults.results.length }}</p>
-        </div>
-      </template>
-    </Card>
+    <h1 class="title-h1">
+      Risultati del sondaggio "{{ surveyResults.surveyTitle }}"
+    </h1>
 
-    <div>
+    <div v-if="surveyResults && surveyResults.results" class="users-div">
+      <Tag class="users-tag">{{ getTotalResponses() }} utenti hanno risposto al sondaggio</Tag>
+    </div>
+
+    <!-- <div>
       <div
         v-for="(question, qIndex) in surveyResults.results"
         :key="question.questionId"
         class="mb-6"
       >
         <Card class="mb-3">
-          <template #title> Domanda {{ qIndex + 1 }} </template>
+          <template #title> Domanda {{ qIndex + 1 }}: {{ question.questionText }} </template>
           <template #content>
             <div v-for="option in question.options" :key="option.optionId" class="mb-2">
               <div class="option-div">
-                <span>Opzione:{{ option.optionId }}</span>
+                <span>{{ option.optionText }}</span>
                 <span>Voti:{{ option.votes }}</span>
               </div>
               <ProgressBar :value="calculatePercentage(option.votes, question.options)" />
@@ -94,15 +102,56 @@ onMounted(fetchSurveyResults)
           </template>
         </Card>
       </div>
+    </div> -->
+    <div
+      v-for="(question, qIndex) in surveyResults.results"
+      :key="question.questionId"
+      class="survey-result-div"
+    >
+      <h3 class="question-h3">Domanda {{ qIndex + 1 }}: {{ question.questionText }}</h3>
+      <div v-for="option in question.options" :key="option.optionId">
+        <div class="option-div">
+          <span>{{ option.optionText }}</span>
+          <span>Voti: {{ option.votes }}</span>
+        </div>
+        <ProgressBar :value="calculatePercentage(option.votes, question.options)" />
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.title-h1{
+  margin: 1rem;
+}
+
+.question-h3{
+  margin: 0;
+  padding-bottom: 1rem;
+}
+
+.users-div {
+  padding-left: 1rem;
+}
+
+.users-tag {
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+
 .option-div {
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin-bottom: 0.5rem;
+  padding-top: 0.8rem;
+}
+
+.survey-result-div {
+  margin-bottom: 2rem;
+  padding: 2rem;
+  border-radius: 0.5rem;
+  border: 1px solid #e0e0e0;
+  margin: 1rem;
 }
 </style>
--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -8,7 +8,7 @@ import { APP_ROUTES } from '@/constants/APP_ROUTES'
       <!-- SVG identico, ma rovesciato -->
       <svg viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
         <!-- Livello 1 -->
-        <path fill="#e6ccf2" fill-opacity="0.5">
+        <path fill="#9fa8da" fill-opacity="0.5">
           <animate
             attributeName="d"
             dur="12s"
@@ -19,7 +19,7 @@ import { APP_ROUTES } from '@/constants/APP_ROUTES'
               M0,160 C360,240 1080,80 1440,160 L1440,320 L0,320 Z;"
           />
         </path>
-        <path fill="#c8a2c8" fill-opacity="0.7">
+        <path fill="#7986cb" fill-opacity="0.7">
           <animate
             attributeName="d"
             dur="16s"
@@ -30,7 +30,7 @@ import { APP_ROUTES } from '@/constants/APP_ROUTES'
               M0,180 C400,260 1040,100 1440,180 L1440,320 L0,320 Z;"
           />
         </path>
-        <path fill="#a36ca6" fill-opacity="1">
+        <path fill="#3f51b5" fill-opacity="1">
           <animate
             attributeName="d"
             dur="20s"
@@ -48,7 +48,7 @@ import { APP_ROUTES } from '@/constants/APP_ROUTES'
     <div class="animated-bg bottom">
       <svg viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
         <!-- Stesso SVG del blocco sopra -->
-        <path fill="#e6ccf2" fill-opacity="0.5">
+        <path fill="#9fa8da" fill-opacity="0.5">
           <animate
             attributeName="d"
             dur="12s"
@@ -59,7 +59,7 @@ import { APP_ROUTES } from '@/constants/APP_ROUTES'
               M0,160 C360,240 1080,80 1440,160 L1440,320 L0,320 Z;"
           />
         </path>
-        <path fill="#c8a2c8" fill-opacity="0.7">
+        <path fill="#7986cb" fill-opacity="0.7">
           <animate
             attributeName="d"
             dur="16s"
@@ -70,7 +70,7 @@ import { APP_ROUTES } from '@/constants/APP_ROUTES'
               M0,180 C400,260 1040,100 1440,180 L1440,320 L0,320 Z;"
           />
         </path>
-        <path fill="#a36ca6" fill-opacity="1">
+        <path fill="#3f51b5" fill-opacity="1">
           <animate
             attributeName="d"
             dur="20s"
