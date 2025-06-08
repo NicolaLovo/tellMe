e4b143030ec7dbf234799380933e69203c6c6d7e
NickLech <niccololechthaler@gmail.com>
2025-06-08 08:36:31 +0200

    Quiz test

--- server/src/__tests__/quizzes.test.ts
+++ server/src/__tests__/quizzes.test.ts
@@ -1,11 +1,17 @@
 import request from 'supertest';
 import app from '../app';
-import { UserModel } from '../database/auth/UserSchema';
 import { connectToDatabase } from '../database/connectToDatabase';
-import { getTestUserFirebaseToken } from './utils/auth/getUsersFirebaseTokens';
+import { loginTestUser } from './utils/auth/loginUser';
 import { TEST_USERS } from './utils/constants/TEST_USERS';
 import { initFirebaseClient } from './utils/database/testFirebaseClient';
 import { initFirebaseServer } from './utils/database/testFirebaseServer';
+import { jwtDecode } from 'jwt-decode';
+
+interface TokenPayload {
+  uid: string;
+  email: string;
+  roles: string[];
+}
 
 describe('Quizzes Tests', () => {
   let townCouncilToken = '';
@@ -20,36 +26,15 @@ describe('Quizzes Tests', () => {
     await initFirebaseServer();
     await initFirebaseClient();
 
-    // Login town council
-    const townCouncilLoginRes = await request(app)
-      .post('/api/v1/auth/login')
-      .send({ firebaseToken: townCouncilFirebaseToken });
-
-
-    // Token Firebase
-    const agencyCredentials = await getTestUserFirebaseToken({
+    agencyToken = await loginTestUser({
       email: TEST_USERS.agency.email,
       password: TEST_USERS.agency.password,
     });
-    const townCouncilCredentials = await getTestUserFirebaseToken({
-      email: TEST_USERS.townCouncil.email,
-      password: TEST_USERS.townCouncil.password,
-    });
 
-
-    // Register agency
-    await request(app)
-      .post('/api/v1/auth/agencies')
-      .send({
-        email: TEST_USERS.agency.email,
-        firebaseToken: agencyFirebaseToken,
-      })
-      .set('Authorization', `Bearer ${townCouncilToken}`);
-
-    // Login agency
-    const agencyLoginRes = await request(app)
-      .post('/api/v1/auth/login')
-      .send({ firebaseToken: agencyFirebaseToken });
+    const decoded = jwtDecode<TokenPayload>(agencyToken);
+    agencyId = decoded.uid;
+    console.log('agency id --> ' , agencyId);
+    
   });
 
   test('POST /api/v1/:agencyId/quizzes should return 200 with valid quiz', async () => {
